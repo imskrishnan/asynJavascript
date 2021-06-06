491bb83095ca036a6e192727d20824e03d034a61
Santhosh Krishnan <imskrishnan@gmail.com>
2021-06-06 09:24:06 -0400

    Async example code

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 //synchronous javascript
 
-const anotherFunction = () => {
-  console.log('Inside another function..');
-  console.log('Do something!');
-};
+// const anotherFunction = () => {
+//   console.log('Inside another function..');
+//   console.log('Do something!');
+// };
 
-console.log('Start');
+// console.log('Start');
 
-//calling anotherFunction()
-anotherFunction();
+// //calling anotherFunction()
+// anotherFunction();
 
-console.log("Adding end from remote..")
+// console.log("Adding end from remote..")
+
+// Asynchronous code
+console.log('Start..');
+
+setTimeout(() => {
+  console.log('We are in the timeout...');
+}, 2000); // this code runs only after 2 seconds..
+
+console.log('End...');
